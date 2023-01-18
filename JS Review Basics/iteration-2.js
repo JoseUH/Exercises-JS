af1f4b12405e31421b49015dc250df312b7bc201
JoseUH <garcia.arencon@bootcamp-upgrade.com>
2023-01-18 15:12:45 +0100

    feat

--- JS Review Basics/iteration-2.js
+++ JS Review Basics/iteration-2.js
@@ -78,3 +78,4 @@ console.log(mediaVolume= mediaVolume / contador)
 
 
 
+
--- JS Review Basics/iteration-6.js
+++ JS Review Basics/iteration-6.js
@@ -5,10 +5,14 @@ const jugadores = [
   "Ronalguiño",
 ];
 
-function swap(array, param1, param2) {
- 
-  array.splice(param1, 1, param2);
-    
+function swap(array, firstIndex, secondIndex) {
+  //voy a guardar para despues el valor
+  const primerElement = array[firstIndex];
+  // ahora le voy a decir que el esta posicion, ahora va a valer lo que valia la otra posición
+  array[firstIndex] = array[secondIndex];
+  //yo que me habia guardado el valor de mi primer elemento, le voy a decir que la posicion del segundod elemento, es ahora la del primero
+  array[secondIndex] = primerElement;
+  return array;
 }
-swap(jugadores, "Mesirve", "Ronalguiño");
+swap(jugadores, 0, 2);
 console.log(jugadores);
